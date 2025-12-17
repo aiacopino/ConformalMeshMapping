307dc0fb495b623ed6c9bfdf68f8779e8b742bae
Alessandra Iacopino <aiacopino@ethz.ch>
2025-12-17 21:20:35 +0100

    wegmann solver start

--- code/src/wegmann.py
+++ code/src/wegmann.py
@@ -1,24 +1,63 @@
-'''
+import numpy as np
+import src.operators as ops
 from targetregion import BoundaryCurve
-## wegmann's method algorithm for conformal mapping
-input: boundary parametrisation eta of target region, anchoring points z_0 on unit disk, s_0 in [0,2pi]
-output: conformal map psi from unit disk to target region
+import functools
 
 
 class WegmannSolver:
-        #perform one newton iter
-    def step():
+    '''
+    wegmann's method algorithm for conformal mapping
+    input: boundary parametrisation eta of target region, anchoring points z_0 on unit disk, s_0 in [0,2pi]
+    output: conformal map psi from unit disk to target region
+    '''
+    def __init__(self, eta, N):
+        self.eta = eta
+        self.N = N
+
+        # fix normalization psi(0) = eta(s_0) for uniqueness
+        ################### check this ######################
+        self.z_0 = eta.evaluate(0)
+        self.s_0 = 0
+
+        #state variables
+        self.current_approximation_coeffs = None
+        self.current_derivative_coeffs = None
+        self.error_history = [] # convergence tracking
+
+    def init_initial_guess(self):
+        pass
+
+
+    def step(self):
+        #perform one newton iteration
         # eval boundary geometry
-        f = eta.evaluate(s)
-        Df = eta.derivative(s)
+        f = self.eta
+        g = ops.fourier_derivative(f)
         # RH problem
-        RHS = (f/Df).imag
+        RHS = (f/g).imag
         LHS = None
         pass
-    def solve():
-        #iterate until convergenceorget about energy management and only focus on solving 
-        pass
+
+    def solve(self, max_iter = 100, epsilon = 1e-6):
+        #iterate until convergence
+        self.init_initial_guess()
+        for i in range(max_iter):
+            err = self.step()
+            self.error_history.append(err)
+            if err < epsilon:
+                print(f"Converged in {i} iterations. \n Error history: {self.error_history}")
+                return self.current_approximation_coeffs
+        print(f"Max iterations reached. \n Error history: {self.error_history}")
+        return self.current_approximation_coeffs
+
     def count_iterations():
+        pass
+
+    @functools.cached_property
+    def current_tangent_angle(self):
+        # calc current tangent angle from current derivative coeffs
+        # cached property so only calculated once per iteration
+
         pass
 
 ## initial guess for boundary correspondence function S
@@ -32,4 +71,3 @@ class WegmannSolver:
 ## make entire mesh from boundary
 
 ## count iterations/ convergence rate
-'''
